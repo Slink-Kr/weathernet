20a464fa2738e93ae1ab742ae70a831adff5497f
Dang Kieu <kieu@kachingcash.com>
2017-07-11 15:43:59 +0700

    modify some style source

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -25790,6 +25790,8 @@ var _react = __webpack_require__(7);
 
 var _react2 = _interopRequireDefault(_react);
 
+__webpack_require__(227);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -25892,8 +25894,6 @@ var _createClass = function () { function defineProperties(target, props) { for 
 var _react = __webpack_require__(7);
 
 var _react2 = _interopRequireDefault(_react);
-
-__webpack_require__(227);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './../sass/Home.scss'
 
 export default class Home extends Component {
     constructor(props) {
